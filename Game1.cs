787bd9f5bf0cfa566871989745e881b9bee2fb2b
Jackson Majewski <jacksonmajewski@gmail.com>
2022-02-24 22:28:26 -0500

    Now has same functionality as SLD

--- Game1.cs
+++ Game1.cs
@@ -1,13 +1,58 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace NotSoSimpleLevelDesigner
 {
+    public enum ProgramState
+    {
+        Setup,
+        Edit,
+        Done
+    }
+    public enum EditorState
+    {
+        Walls,
+        InvisibleWalls,
+        Mirrors,
+        Enemies,
+        Player
+    }
+
     public class Game1 : Game
     {
+       /*
+        * Level Key:
+        * 
+        * 0 - Empty
+        * W - Wall
+        * I - Invisible Wall
+        * M - Mirror
+        * E - Enemy
+        * P - Player
+        * 
+        */
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        //Variables
+        string userInput;
+        int columns;
+        int rows;
+        string filepath;
+        char[,] level;
+        LevelManager levelManager;
+        string welcome = @"   _____                   _        _   Welcome to the   _   _____            _
+  / ____\     (not so)    | |      | |                  | | |  __ \          (_)                      
+ | (___  _ _ __ ___  _ __ | | ___  | |     _____   _____| | | |  | | ___  ___ _  __ _ _ __   ___ _ __ 
+  \___ \| | '_ ` _ \| '_ \| |/ _ \ | |    / _ \ \ / / _ \ | | |  | |/ _ \/ __| |/ _` | '_ \ / _ \ '__|
+  ____) | | | | | | | |_) | |  __/ | |___|  __/\ V /  __/ | | |__| |  __/\__ \ | (_| | | | |  __/ |   
+ |_____/|_|_| |_| |_| .__/|_|\___| |______\___| \_/ \___|_| |_____/ \___||___/_|\__, |_| |_|\___|_|   
+                    | |                                                          __/ |                
+                    |_|               by Jackson Majewski                       |___/                 
+";
 
         public Game1()
         {
@@ -16,9 +61,57 @@ namespace NotSoSimpleLevelDesigner
             IsMouseVisible = true;
         }
 
+        public void GenerateWalls()
+        {
+            //Top row
+            for (int i = 0; i < columns; i++)
+                level[0, i] = 'W';
+
+            for (int i = 1; i < rows - 1; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j == 0 || j == columns - 1)
+                    {
+                        level[i, j] = 'W';
+                    }
+                    else
+                        level[i, j] = '0'; //Not a wall
+                }
+            }
+
+            //Bottom row
+            for (int i = 0; i < columns; i++)
+                level[rows - 1, i] = 'W';
+
+        }
+
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            //Print welcome message
+            Console.WriteLine(welcome + "\n");
+
+            System.IO.Directory.CreateDirectory("levels"); //Create levels directory if it doesn't exist
+
+            //Set level file
+            Console.Write("Level Name> ");
+            userInput = Console.ReadLine();
+            filepath = "levels\\" + userInput + ".sslvl";
+
+            //Get width of room in tiles
+            Console.Write("width> ");
+            userInput = Console.ReadLine();
+            columns = int.Parse(userInput);
+
+            //Get height of room in tiles
+            Console.Write("height> ");
+            userInput = Console.ReadLine();
+            rows = int.Parse(userInput);
+
+            level = new char[rows, columns];
+            levelManager = new LevelManager(filepath);
+            GenerateWalls();
+            levelManager.SaveFile(level);
 
             base.Initialize();
         }
---
+++ LevelManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace NotSoSimpleLevelDesigner
+{
+    class LevelManager
+    {
+        //Fields
+        private string filepath;
+        private StreamWriter output;
+
+        //Constructor
+        public LevelManager(string filepath)
+        {
+            this.filepath = filepath;
+        }
+
+        //Methods
+        public void SaveFile(char[,] level)
+        {
+            try {
+                output = new StreamWriter(filepath);
+
+                output.WriteLine("{0}, {1}", level.GetLength(0), level.GetLength(1));
+
+                for (int row = 0; row < level.GetLength(0); row++)
+                {
+                    for (int column = 0; column < level.GetLength(1); column++)
+                    {
+                        output.Write(level[row, column]);
+                    }
+                    output.WriteLine();
+                }
+                output.Close();
+            }
+            catch (Exception)
+            {
+                output.Close();
+                Console.WriteLine("Error saving file.");
+            }
+        }
+    }
+}
